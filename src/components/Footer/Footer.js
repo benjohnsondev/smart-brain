d02f171521043309a54e05edc8225c2bce6a946c
Ben Johnson <benjohnsondevelopment@gmail.com>
2019-09-25 13:01:49 +0100

    adding files

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Footer = () => {
+  const greeting = "Ben Johnson";
+  return (
+  <div>
+  <p className="pt7">Made with <span style={{ color: '#e25555'}}>&#9829;</span> by {greeting}</p>
+  </div>
+);
+};
+
+
+
+export default Footer;
