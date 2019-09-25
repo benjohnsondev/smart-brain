eb685ca915a6a14442a8b5d2ad2c3ca6a154ccb0
Ben Johnson <benjohnsondevelopment@gmail.com>
2019-09-25 13:11:53 +0100

    added cors

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3000/imageurl', {
+      fetch('http://cryptic-hamlet-56946.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -86,7 +86,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('http://cryptic-hamlet-56946.herokuapp.com:3000/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
